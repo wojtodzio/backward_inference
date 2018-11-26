9198190338cbb9a7350171dfb88732eff300e3e8
Wojtek Wrona <wojtodzio@gmail.com>
2018-11-26 12:33:35 +0100

    Pause after tests on windows

--- src/variable.rb
+++ src/variable.rb
@@ -4,9 +4,7 @@ class Variable
   include Dry::Equalizer(:identity_string)
 
   def initialize(original_name)
-    @original_name = original_name
-
-    random_number_for_display ||= SecureRandom.random_number(1000)
+    @original_name   = original_name
     @identity_string = SecureRandom.uuid # Use random string with a very very low chance of clashing
   end
 
@@ -14,10 +12,6 @@ class Variable
     original_name
   end
   alias_method :inspect, :to_s
-
-  def variable?
-    true
-  end
 
   private
 
